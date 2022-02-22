4fdad36cf5ebbe80e96991086c48e188be974b42
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-22 14:09:17 +0800

    buy和sell

--- trader.py
+++ trader.py
@@ -25,12 +25,17 @@ class Trader:
   def buy(
     self,
     kline: KLine,
-    position: int = 1,
+    percent: int = 1,
   ):
-    if self.funds > 0 and position > 0 and kline.close > 0:
-      useFunds = self.funds * position
+    if (
+      self.funds > 0 and
+      percent > 0 and
+      percent <= 1 and
+      kline.close > 0
+    ):
+      useFunds = self.funds * percent
       self.funds -= useFunds
-      self.assets += useFunds / kline.close * (1 - self.buyFee)
+      self.assets += (useFunds / kline.close) * (1 - self.buyFee)
       return True
     return False
       
@@ -38,9 +43,19 @@ class Trader:
   def sell(
     self,
     kline: KLine,
-    position: int = 1,
+    percent: int = 1,
   ):
-    pass
+    if (
+      self.assets > 0 and
+      percent > 0 and
+      percent <= 1 and
+      kline.close >= 0
+    ):
+      useAssets = self.assets * percent
+      self.assets -= useAssets
+      self.funds += (useAssets * kline.close) * (1 - self.sellFee)
+      return True
+    return False
   
   def long(
     self,
