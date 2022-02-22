1f7551c6524f63d95929b8eb0763237364b14451
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-22 16:11:49 +0800

    提交

--- dev.py
+++ dev.py
@@ -6,4 +6,10 @@ from tools.yfinance import load
 df = load('BTC-USD')
 df.ta.sma(length = 8, append = True)
 df.ta.sma(length = 44, append = True)
-print(df.iloc[0:5])
+print(df.iloc[0:0])
+print(len(df.index))
+
+for index in range(len(df.index)):
+  newDf = df.iloc[0:index + 1]
+  print(len(newDf.index))
+  pass
