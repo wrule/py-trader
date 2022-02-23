91f765ce175caee3f9e89298f485bba584fe1cb0
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-23 15:55:48 +0800

    dataframe插入

--- dev.py
+++ dev.py
@@ -6,7 +6,7 @@ from tools.yfinance import load
 from strategy.strategy import Strategy
 from strategy.twoLinesCross import TwoLinesCross
 
-# df = load('BTC-USD')
+df1 = load('BTC-USD')
 # df.ta.sma(length = 8, append = True)
 # df.ta.sma(length = 44, append = True)
 
@@ -14,3 +14,12 @@ from strategy.twoLinesCross import TwoLinesCross
 # s.Backtesting(df)
 
 print('你好，世界')
+
+df = pd.DataFrame()
+newRow = pd.DataFrame({
+  'age': range(10),
+  'name': [f'第{x + 1}个' for x in range(10)],
+})
+# df.append(newRow)
+print(newRow)
+# print(df1)
