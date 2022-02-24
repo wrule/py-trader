45046a3284bcf51e2d5ea06ca8a19f5fb276c519
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-24 09:41:27 +0800

    更新

--- snapshot_list.py
+++ snapshot_list.py
@@ -21,15 +21,22 @@ class Snapshot:
 
 class SnapshotList:
   list: List[Snapshot] = []
+  
+  updated: bool = False
+  df: DataFrame = None
 
   def append(self, snapshot: Snapshot):
     self.list.append(snapshot)
+    self.updated = False
 
   def dataframe(self):
-    df = DataFrame(self.list)
-    df.index = pd.to_datetime(df['datetime'])
-    df.drop('datetime', axis = 1, inplace = True)
-    df['assetValuation'] = df.apply(lambda row: row['funds'] + row['assets'] * row['price'], axis = 1)
-    df['debtValuation'] = df.apply(lambda row: row['fundsDebt'] + row['assetsDebt'] * row['price'], axis = 1)
-    df['valuation'] = df.apply(lambda row: row['assetValuation'] - row['debtValuation'], axis = 1)
-    return df
+    if not self.updated:
+      df = DataFrame(self.list)
+      df.index = pd.to_datetime(df['datetime'])
+      df.drop('datetime', axis = 1, inplace = True)
+      df['assetValuation'] = df.apply(lambda row: row['funds'] + row['assets'] * row['price'], axis = 1)
+      df['debtValuation'] = df.apply(lambda row: row['fundsDebt'] + row['assetsDebt'] * row['price'], axis = 1)
+      df['valuation'] = df.apply(lambda row: row['assetValuation'] - row['debtValuation'], axis = 1)
+      self.df = df
+      self.updated = True
+    return self.df
