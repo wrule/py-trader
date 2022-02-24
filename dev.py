9c4c217736088e12444d658383a821c2f8719ee6
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-24 18:14:55 +0800

    一些修改

--- dev.py
+++ dev.py
@@ -1,34 +1,19 @@
 #!/opt/homebrew/bin/python3
-from cmath import nan
+from datetime import datetime
 import pandas as pd
 import pandas_ta as ta
 from tools.yfinance import load
-from strategy.strategy import Strategy
 from strategy.twoLinesCross import TwoLinesCross
-from snapshot_list import Snapshot, SnapshotList
-import datetime
 
 from trader import Trader
 
-df1 = load('BTC-USD')
-df1.ta.sma(length = 8, append = True)
-df1.ta.sma(length = 44, append = True)
-# print(df1.iloc[0])
-# a = df1.iloc[0]
-# for index, row in df1.iterrows():
-#   print(row['Close'])
+df = load('BTC-USD')
+df.ta.sma(length = 8, append = True)
+df.ta.sma(length = 44, append = True)
 
-print(df1.columns)
-
-# trader = Trader()
-# s = TwoLinesCross(trader, 'SMA_8', 'SMA_44')
-# s.Backtesting(df1)
-# print(trader.funds)
-
-# print('你好，世界')
-
-# list = SnapshotList()
-# list.append(Snapshot(0))
-# list.append(Snapshot(100000000000000, 1, 2, 0, 0, 2))
-# list.append(Snapshot(200000000000000, 2, 2, 0, 0, 1.9))
-# print(list.dataframe())
+trader = Trader()
+tlc = TwoLinesCross(trader, 'SMA_8', 'SMA_44')
+print(datetime.now())
+tlc.Backtesting(df)
+print(datetime.now())
+print(trader.funds)
--- tools/yfinance.py
+++ tools/yfinance.py
@@ -5,7 +5,8 @@ import yfinance as yf
 def download(symbol: str):
   print(f'{symbol}数据下载中...')
   ticker = yf.Ticker(symbol)
-  hist = ticker.history(period = 'max')
+  hist: pd.DataFrame = ticker.history(period = 'max')
+  hist['Date'] = hist.index
   filePath = f'data/yahoo/{symbol}.pkl'
   hist.to_pickle(filePath)
   print(f'下载完成，数据已经保存在: {filePath}')
