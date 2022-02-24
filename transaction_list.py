c05d5f1e002b5bf09e9dc9b1c096203ecc0534e9
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-24 10:18:39 +0800

    交易列表

--- transaction_list.py
+++ transaction_list.py
@@ -1,9 +1,21 @@
 
 from dataclasses import dataclass
+from datetime import datetime
 
 @dataclass
 class Transaction:
-  pass
+  beforeDatetime: datetime
+  afterDatetime: datetime
+  beforeFunds: float = 0
+  beforeAssets: float = 0
+  beforeFundsDebt: float = 0
+  beforeAssetsDebt: float = 0
+  beforePrice: float = 0
+  afterFunds: float = 0
+  afterAssets: float = 0
+  afterFundsDebt: float = 0
+  afterAssetsDebt: float = 0
+  afterPrice: float = 0
 
 class TransactionList:
   def __init__(self):
