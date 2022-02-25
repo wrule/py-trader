30a1fd347f3cdde8bba2cd7c45b1606801187ab3
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-25 15:00:05 +0800

    matplotlib显示资金图

--- dev.py
+++ dev.py
@@ -1,9 +1,13 @@
 #!/opt/homebrew/bin/python3
 from datetime import datetime
+import time
 import pandas_ta as ta
 from tools.yfinance import load, to_dict_list
 from strategy.twoLinesCross import TwoLinesCross
 from trader import Trader
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
 
 df = load('BTC-USD')
 df.ta.sma(length = 8, append = True)
@@ -19,3 +23,11 @@ print(datetime.now() - oldTime)
 print(trader.funds)
 trader.transactionList.dataframe().to_excel('transaction.xlsx')
 trader.snapshotList.dataframe().to_excel('snapshot.xlsx')
+
+df = trader.snapshotList.dataframe()
+# print(df)
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot(df.index, df['valuation']);  # Plot some data on the axes.
+plt.show()
+
