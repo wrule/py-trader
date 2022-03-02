d931a76e8e8de24b8d8afca0da392f35e8cdafe6
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-02 11:09:53 +0800

    策略就是指标集合

--- dev.py
+++ dev.py
@@ -1,10 +1,21 @@
 #!/opt/homebrew/bin/python3
+from pprint import pprint
+from numpy import append
 import pandas as pd
 import pandas_ta as ta
 from tools.yfinance import load
+import pprint
 
-df = load('BTC-USD')
-df.ta.log_return(cumulative = True, append = True)
-df.ta.percent_return(cumulative = True, append = True)
-df.ta.ema(length = 20, append = True)
-print(df.tail(10))
+if __name__ == '__main__':
+  df = load('BTC-USD')
+  strategy = ta.Strategy(
+    name = 'JIMAO',
+    ta = [
+      { 'kind': 'sma', 'length': 8, },
+      { 'kind': 'sma', 'length': 44, }
+    ],
+  )
+  df.ta.strategy(ta.CommonStrategy)
+  # print(df.tail(5))
+  print(df.columns)
+# pprint.pprint('123')
