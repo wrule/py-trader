d74ee268b392027ca915ece1e99b787a2e269a9d
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-02 08:59:27 +0800

    基本数据显示

--- dev.py
+++ dev.py
@@ -1,6 +1,13 @@
 #!/opt/homebrew/bin/python3
-import pandas as pd
-from tools.tradingview_csv import load
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import mplcursors
 
-df = load('BINANCE_BTCUSDT, 120')
-print(df)
+fig, ax = plt.subplots()
+fig.set_size_inches(8, 4)
+fig.canvas.manager.set_window_title('简单折线图')
+ax.plot([0, 1, 2, 3, 4], [1, 4, 2, 3, 9]);
+ax.legend()
+mplcursors.cursor(hover = True)
+plt.show()
