17b2697c0199aa41ea00708a4151864ffc1c77f5
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-03 16:12:32 +0800

    重新思考

--- dev.py
+++ dev.py
@@ -1,19 +1,6 @@
 #!/opt/homebrew/bin/python3
-from pprint import pprint
-from numpy import append
-import pandas as pd
-import pandas_ta as ta
 from tools.yfinance import load, download
-import pprint
 
 if __name__ == '__main__':
   df = load('BTC-USD')
-  strategy = ta.Strategy(
-    name = 'JIMAO',
-    ta = [
-      { 'kind': 'sma', 'length': 8, },
-      { 'kind': 'sma', 'length': 44, }
-    ],
-  )
-  df.ta.strategy(strategy)
-  print(df.tail(10))
+  print(df)
