830b0bc52c4443222752a6163260465b39c47679
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-04 08:50:33 +0800

    fix: 提交代码

--- dev.py
+++ dev.py
@@ -1,4 +1,5 @@
 #!/opt/homebrew/bin/python3
+import time
 import pandas_ta as ta
 from tools.yfinance import load, to_dict_list
 from trader import Trader
@@ -8,9 +9,10 @@ if __name__ == '__main__':
   df = load('BTC-USD')
   df.ta.sma(length = 8, append = True)
   df.ta.sma(length = 44, append = True)
-  print(df)
   hist = to_dict_list(df)
   trader = Trader()
   strategy = TwoLinesCross(trader, 'SMA_8', 'SMA_44')
+  oldTime = time.perf_counter()
   strategy.Backtesting(hist)
+  print(time.perf_counter() - oldTime)
   print(trader.snapshotList.last().valuation())
