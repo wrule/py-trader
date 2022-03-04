5e96a68f749b2b662f3978b58d83d83b1c569c24
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-04 15:56:21 +0800

    buy shell

--- numba_try/trader.py
+++ numba_try/trader.py
@@ -1,4 +1,5 @@
 from numba.experimental import jitclass
+from numba_try.kline import KLine
 
 @jitclass
 class Trader:
@@ -27,3 +28,36 @@ class Trader:
     self.assets = 0
     self.assetsDebt = 0
 
+  def buy(
+    self,
+    kline: KLine,
+    percent: int = 1,
+  ):
+    if (
+      self.funds > 0 and
+      percent > 0 and
+      percent <= 1 and
+      kline.close > 0
+    ):
+      useFunds = self.funds * percent
+      self.funds -= useFunds
+      self.assets += (useFunds / kline.close) * (1 - self.buyFee)
+      return True
+    return False
+  
+  def sell(
+    self,
+    kline: KLine,
+    percent: int = 1,
+  ):
+    if (
+      self.assets > 0 and
+      percent > 0 and
+      percent <= 1 and
+      kline.close >= 0
+    ):
+      useAssets = self.assets * percent
+      self.assets -= useAssets
+      self.funds += (useAssets * kline.close) * (1 - self.sellFee)
+      return True
+    return False
