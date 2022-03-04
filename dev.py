dbf1fec9c0f9379745396ed25b04357fad5fd7d6
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-04 11:11:13 +0800

    jit优化

--- dev.py
+++ dev.py
@@ -1,18 +1,23 @@
 #!/opt/homebrew/bin/python3
 import time
-import pandas_ta as ta
-from tools.yfinance import load, to_dict_list
-from trader import Trader
-from strategy.twoLinesCross import TwoLinesCross
+from numba import jit, njit, prange
+
+@jit
+def testFor(n: int, m: int):
+  for x in prange(n):
+    for y in prange(m):
+      pass
+
+@jit
+def testWhile(n: int, m: int):
+  y = 0
+  while y < n:
+    x = 0
+    while x < m:
+      x = x + 1
+    y = y + 1
 
 if __name__ == '__main__':
-  df = load('BTC-USD')
-  df.ta.sma(length = 8, append = True)
-  df.ta.sma(length = 44, append = True)
-  hist = to_dict_list(df)
-  trader = Trader()
-  strategy = TwoLinesCross(trader, 'SMA_8', 'SMA_44')
   oldTime = time.perf_counter()
-  strategy.Backtesting(hist)
+  testFor(20100, 2800)
   print(time.perf_counter() - oldTime)
-  # print(trader.snapshotList.last().valuation())
