ae9f994eaff8e3175ba9d3dddbb81128a536fc54
jimao <jimao@jimaodeMBP.lan>
2022-03-06 13:46:27 +0800

    dev

--- dev.py
+++ dev.py
@@ -1,6 +1,7 @@
 #!/opt/homebrew/bin/python3
+from numpy import append
 import pandas_ta as ta
-from tools.yfinance import load, to_kline_list
+from tools.yfinance import load, to_dict_list
 from trader import Trader
 from strategy.twoLinesCross import TwoLinesCross
 from strategy.history import History
@@ -8,23 +9,16 @@ import time
 
 if __name__ == '__main__':
   df = load('BTC-USD')
-  for n in range(2, 201):
-    df.ta.sma(length = n, append = True)
-  klines = to_kline_list(df)
-  index = 0
-  for num in df['SMA_8']:
-    klines[index].data['SMA_8'] = num
-    index = index + 1
-  index = 0
-  for num in df['SMA_44']:
-    klines[index].data['SMA_44'] = num
-    index = index + 1
-  hist = History(klines)
+  for size in range(2, 201):
+    df.ta.sma(length = size, append = True)
+  df['SMA_1'] = df['Close']
+  hist = History(to_dict_list(df))
   trader = Trader()
-  strategy = TwoLinesCross(trader, 'SMA_8', 'SMA_44')
   oldTime = time.perf_counter()
-  for i in range(201):
-    strategy.backtesting(hist)
+  for fast in range(1, 200):
+    for slow in range(fast + 1, 201):
+      strategy = TwoLinesCross(trader, f'SMA_{fast}', f'SMA_{slow}')
+      strategy.backtesting(hist)
   print(time.perf_counter() - oldTime)
   print(trader.snapshotList.last().valuation())
   print(trader.transactionList.length())
