b919f235db594ed1579f68d0a20e3175555fca97
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-07 18:19:00 +0800

    随机化优化

--- optimizer/int_random.py
+++ optimizer/int_random.py
@@ -1,8 +1,16 @@
+from typing import Callable
+from int_space import IntSpace
 
 class Random:
   def __init__(
     self,
+    space: IntSpace,
   ):
     pass
   
-  
+  def explore(
+    self,
+    func: Callable([], float),
+  ):
+    a = func()
+    pass
