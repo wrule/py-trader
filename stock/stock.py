46f35b7b682fa28153f3e29c6d642f6fce9cbffd
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-10 16:58:34 +0800

    提交

--- stock/stock.py
+++ stock/stock.py
@@ -1,28 +1,28 @@
 from datetime import datetime
 from abc import ABC, abstractmethod
+from typing import Any, Dict
 
 class Stock(ABC):
   def __init__(
     self,
     volume: float,
-    restockTime: datetime,
-    restockPrice: float,
+    data: Dict[str, Any],
   ):
     self.volume = volume
-    self.restockTime = restockTime
-    self.restockPrice = restockPrice
+    self.restockTime = data['Date']
+    self.restockPrice = data['Close']
   
   volume: float
   restockTime: datetime
   restockPrice: float
 
   @abstractmethod
-  def start_valuation(self):
-    return 0
+  def start_valuation(self) -> float:
+    pass
 
   @abstractmethod
-  def current_valuation(self):
-    return 0
+  def current_valuation(self, data: Dict[str, Any]) -> float:
+    pass
 
   def profit(self):
     return self.current_valuation() - self.start_valuation()
