b055fef7e2d152f563741a11da57f5d2e5a593f9
jimao <jimao@jimaodeMBP.lan>
2022-03-10 22:59:12 +0800

    short

--- stock/short.py
+++ stock/short.py
@@ -1,9 +1,20 @@
 from typing import Any, Dict
+from abc import ABC, abstractmethod
 
-class Short:
+class Short(ABC):
   def __init__(
     self,
     volume: float,
     data: Dict[str, Any],
+    lever: int,
   ):
     super().__init__(volume, data)
+    self.lever = lever
+    
+  lever: int
+  
+  def start_valuation(self) -> float:
+    return 0
+  
+  def current_valuation(self, data: Dict[str, Any]) -> float:
+    return 0
