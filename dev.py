a3fa3bd3b03b56817cbbc6781d2813b84ec711aa
jimao <jimao@jimaodeMBP.lan>
2022-03-12 11:11:57 +0800

    sell_assets测试完成

--- dev.py
+++ dev.py
@@ -62,16 +62,16 @@ if __name__ == '__main__':
   print(account.buy_assets(0.1, 110, None))
   print(account.buy_assets(0.1, 110, None))
   print(account.buy_funds_percent(0.5, 110, None))
+
+  for spot in account.spotList:
+    print(spot)
+
+  print(account.funds, account.assets)
+  print(account.sell_assets(0.5, 200, None))
   print(account.funds, account.assets)
 
-  print(account.sell_stock_percent(0, 0.5, 200, None))
-  print(account.funds, account.assets)
-  
-  print(account.sell_all(200, None))
-  print(1, account.funds, round(account.assets, 17))
-
-  # for spot in account.spotList:
-  #   print(spot)
+  for spot in account.spotList:
+    print(spot)
   # stock = Contract(
   #   0.003,
   #   39050.0,
