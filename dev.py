e50ce145d38f7e83b39c8b30f8a433033f1de23b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-15 17:09:20 +0800

    你好,世界

--- dev.py
+++ dev.py
@@ -4,15 +4,4 @@ from account.spot_account import SpotAccount
 from snapshot_list import Snapshot, SnapshotList
 
 if __name__ == '__main__':
-  snapshot_list = SnapshotList()
-  snapshot_list.append(Snapshot(None, 1))
-  snapshot_list.append(Snapshot(None, 2))
-  snapshot_list.append(Snapshot(None, 3))
-  snapshot_list.append(Snapshot(None, 4))
-  snapshot_list.append(Snapshot(None, 5))
-  snapshot_list.append(Snapshot(None, 7))
-  snapshot_list.append(Snapshot(None, 7))
-  snapshot_list.append(Snapshot(None, 6))
-  snapshot_list.append(Snapshot(None, 5))
-  snapshot_list.append(Snapshot(None, 10))
-  print(snapshot_list.sharpe_ratio(2))
+  print('你好，世界')
