ea4521682e6a487fedd8d9528f1ddfc59209fa5f
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-15 17:06:39 +0800

    make_snapshot

--- trader.py
+++ trader.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from account.contract_account import ContractAccount
 from account.spot_account import SpotAccount
+from snapshot_list import Snapshot, SnapshotList
 
 class Trader:
   def __init__(
@@ -9,9 +11,11 @@ class Trader:
   ):
     self.contract = contract
     self.spot = spot
+    self.snapshotList = SnapshotList()
 
   contract: ContractAccount
   spot: SpotAccount
+  snapshotList: SnapshotList
   
   def valuation(self, price: float):
     result = 0
@@ -20,3 +24,6 @@ class Trader:
     if self.spot is not None:
       result += self.spot.valuation(price)
     return result
+
+  def make_snapshot(self, date: datetime, price: float):
+    self.snapshotList.append(Snapshot(date, self.valuation(price)))
