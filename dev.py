f5375436eb889b6cadd48d0b4478d6e950f05c78
jimao <jimao@jimaodeMacBook-Pro.local>
2022-03-15 17:12:12 +0800

    加载数据

--- dev.py
+++ dev.py
@@ -2,6 +2,9 @@
 from account.contract_account import ContractAccount
 from account.spot_account import SpotAccount
 from snapshot_list import Snapshot, SnapshotList
+from tools.tradingview_csv import load
 
 if __name__ == '__main__':
+  df = load('BINANCE_BTCUSDT, 120_cd3a1')
   print('你好，世界')
+  print(df)
