3f318f837c8d422069ea92229e0a037237539d3d
jimao <jimao@jimaodeMBP.lan>
2022-03-16 22:31:45 +0800

    可视化

--- strategy/history.py
+++ strategy/history.py
@@ -1,6 +1,8 @@
 from typing import Any, Dict, List
+from matplotlib.pyplot import title
 from pandas import DataFrame
 from tools.yfinance import to_dict_list
+import mplfinance as mpf
 
 class History:
   def __init__(self, df: DataFrame):
@@ -22,5 +24,23 @@ class History:
     return len(self.data)
 
   def show(self):
-    print('可视化显示')
+    df = self.df[5000:5400]
+    print(df)
+    ap2 = [
+      mpf.make_addplot(df['STOCHRSIk_49_8_8_27'], color = 'g', panel = 2),
+      mpf.make_addplot(df['STOCHRSId_49_8_8_27'], color = 'b', panel = 2),
+    ]
+    mpf.plot(
+      df,
+      type = 'candle',
+      volume = True,
+      style = 'binance',
+      # title = 'kline',
+      # figscale = 1,
+      # figratio = (2.5, 1),
+      # ylabel = '你好',
+      datetime_format = '%m-%d %H:%M',
+      main_panel=1,volume_panel=0,
+      addplot=ap2,
+    )
     pass
